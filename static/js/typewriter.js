8c29ce061e39178129a25ab1f3343443258643a8
Henry-2016 <jacob2016henry@gmail.com>
2021-01-06 20:55:58 +0300

    New Beiwa Admin Server Uploads

--- static/js/typewriter.js
+++ static/js/typewriter.js
@@ -9,21 +9,21 @@ let bioinfoarray = ["Bio digester is a modern on site sewer system As feacal mat
 "1-200 users" ]
 
 
-let whybioarray = ["1 There is no to evacuate the bio digester as opposed to the conventional septic tank which must be evacuted at frequent intervals.",
-"2 Bio tank requires no maintaince.",
-"3 It takes small spae.",
-"4 Its faster in the digestion of feacal matter-12 hours",
-"5 Bio tank deosnt torate insects like flies and cockroaches.",
-"6 No bad smell in the home unlike the conventional septic tank.",
-"7 Its an underground system.",
-"8 It doesnt temper with the beauty of the compound.",
-"9 It doesnt affect other services and future like parking and family events.",
-"10 With a bio digester system, there is no risks of accidents.",
-"11 Oil and soap are filtered from goimg imto the soak pit.",
-"12 It is cheaper and paid once.",
-"13 It is a lifetime system.",
-"14 It has a twenty year guarantee.",
-"15 A bio digester is a recommendable even in the wetland compared to the conventional septic tank.",]
+let whybioarray = ["1. There is no to evacuate the bio digester as opposed to the conventional septic tank which must be evacuted at frequent intervals.",
+"2. Bio tank requires no maintaince.",
+"3. It takes small spae.",
+"4. Its faster in the digestion of feacal matter-12 hours",
+"5. Bio tank deosnt torate insects like flies and cockroaches.",
+"6. No bad smell in the home unlike the conventional septic tank.",
+"7. Its an underground system.",
+"8. It doesnt temper with the beauty of the compound.",
+"9. It doesnt affect other services and future like parking and family events.",
+"10. With a bio digester system, there is no risks of accidents.",
+"11. Oil and soap are filtered from goimg imto the soak pit.",
+"12. It is cheaper and paid once.",
+"13. It is a lifetime system.",
+"14. It has a twenty year guarantee.",
+"15. A bio digester is a recommendable even in the wetland compared to the conventional septic tank.",]
  
 
 function StartTypingBioInfoText ()
@@ -35,7 +35,7 @@ function StartTypingBioInfoText ()
             strings:bioinfoarray,
             typeSpeed: 50, 
             loop: true, 
-            backDelay: 900, 
+            backDelay: 1000, 
             backSpeed: 30, 
         }); 
     }
@@ -50,7 +50,7 @@ function StartTypingReasonsText ()
             strings:whybioarray,
             typeSpeed: 50, 
             loop: true, 
-            backDelay: 900, 
+            backDelay: 1000, 
             backSpeed: 30, 
         }); 
     }
